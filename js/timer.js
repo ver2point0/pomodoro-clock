f6db0a9320bbecdde1dba842647191281f891813
John McIntosh II <jmci10@hotmail.com>
2017-12-16 08:49:55 +0000

    write 1st 2 time display functions

--- js/timer.js
+++ js/timer.js
@@ -5,5 +5,23 @@ var work = true;
 var started = false;
 
 $(document).ready(function() {
-    
+  
+  // show chosen times for break and work
+  function displaySetTime(id, input) {
+    var inputHtml = "<p"> + input + "</p>";
+    var idString = "#".concat(id);
+    $(idString).html(inputHtml);
+  }
+  
+  // actual time for work or rest
+  function displayTimer(id, input) {
+    var seconds = input % 60;
+    var minutes = (input - seconds) / 60;
+    if (seconds >= 0 && seconds <= 9) {
+      seconds = "0".concat(seconds);
+    }
+    var inputHtml = "<p>" + minutes + ":" + seconds + "</p>";
+    var idString = "#".concat(idString);
+    $(idString).html(inputHtml);
+  }
 });
